de2d50b5f826463b7948e0705e904d338e829e8d
Oleg Bondarchuk <bozzard@gmail.com>
2017-10-04 13:45:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = object;
+  console.log('newObject = ', newObject);
+  object [key] = value;
+  return newObject;
+}
